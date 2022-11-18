57a018d6953424d460b0239f68481d0acccbe580
Shashank0606 <shashankrathore606@gmail.com>
2022-11-18 09:03:22 +0530

    TAG: Employee present absent

---
+++ employeewage/src/test/java/com/bridgelabz/employeewage/EmployeewageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeewageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
