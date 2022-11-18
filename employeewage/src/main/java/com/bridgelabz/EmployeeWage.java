42526104751a4643a19c69ae3b33143eb9eacae5
Shashank0606 <shashankrathore606@gmail.com>
2022-11-18 09:20:07 +0530

    TAG: Checking Daily Employee wage

---
+++ employeewage/src/main/java/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,11 @@
+package wage.employeewage.src.main.java.com.bridgelabz;
+
+public class EmployeeWage {
+    public static void main(String args[]) {
+        int fullyDayHours = 8;
+        int wagePerHour = 20;
+        int dailyWage = (fullyDayHours * wagePerHour);
+
+        System.out.println("employee is present and his daily wage is " + dailyWage);
+    }
+}
